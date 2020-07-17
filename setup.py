5783142c696adb5c34ab3759aaf5efafd33fb772
Miquel Perello-Nieto <perello.nieto@gmail.com>
2020-07-17 17:07:09 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from distutils.util import convert_path
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+main_ns = {}
+ver_path = convert_path('pycalib/__init__.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setup(
+  name = 'pycalib',
+  packages = ['pycalib'],
+  install_requires=[
+    'numpy',
+    'scipy',
+    'sklearn',
+    'matplotlib',
+  ],
+  version=main_ns['__version__'],
+  description = 'Python library with tools for classifier calibration.',
+  author = 'Miquel Perello Nieto, Hao Song, Telmo de Menezes e Silva Filho',
+  author_email = 'perello.nieto@gmail.com',
+  url = 'https://github.com/Srceh/conditional_sampler',
+  download_url = 'https://github.com/perellonieto/pycalib/archive/{}.tar.gz'.format(main_ns['__version__']),
+  keywords = ['classifier calibration', 'calibration', 'classification'],
+  classifiers = [],
+  long_description=long_description
+)
